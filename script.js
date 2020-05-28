a809fd9760b928f18969cc15e9c9c37b2f6a138a
kaspyar <ykaspryshyn@gamepoint.com>
2020-05-28 21:46:44 +0300

    [undoc] - imported new site

--- script.js
+++ script.js
@@ -1,53 +1,30 @@
-var currentIndex = 0;
-
-document.addEventListener('DOMContentLoaded', function(){
-
-
-
-  var pages = document.getElementsByClassName('page');
+var pages = document.getElementsByClassName('page');
   for(var i = 0; i < pages.length; i++)
-  {
-    var page = pages[i];
-    page.style.zIndex = (pages.length - i);
-  }
+    {
+      var page = pages[i];
+      if (i % 2 === 0)
+        {
+          page.style.zIndex = (pages.length - i);
+        }
+    }
 
-  for(var i = 0; i < pages.length; i++)
-  {
-      //Or var page = pages[i];
-      pages[i].pageNum = i + 1;
-      pages[i].onclick=function(){
-          var forward = true;
-          if (forward == true){
-            this.classList.add('flipped');
-            this.nextElementSibling.classList.add('flipped');
-          } else {
-              this.classList.remove('flipped');
-              this.previousElementSibling.classList.remove('flipped');
-          }
-      }
-  }
-
-    // var prevPage = document.getElementsByClassName('prevPage');
-    // prevPage[0].onclick = function(){
-    //
-    //   console.log('prev page button clicked. cur page index =',currentIndex)
-    //
-    //   if (currentIndex <= 0){
-    //     console.log("At the start")
-    //   } else {
-    //     currentIndex--;
-    //   }
-    // }
-    //
-    // var nextPage = document.getElementsByClassName('nextPage');
-    // nextPage[0].onclick = function(){
-    //
-    //   console.log('next page button clicked. cur page index =',currentIndex)
-    //
-    //   if (currentIndex >= pages.length){
-    //     console.log("At the end")
-    //   } else {
-    //     currentIndex++;
-    //   }
-    // }
-})
+  document.addEventListener('DOMContentLoaded', function(){
+    for(var i = 0; i < pages.length; i++)
+      {
+        //Or var page = pages[i];
+        pages[i].pageNum = i + 1;
+        pages[i].onclick=function()
+          {
+            if (this.pageNum % 2 === 0)
+              {
+                this.classList.remove('flipped');
+                this.previousElementSibling.classList.remove('flipped');
+              }
+            else
+              {
+                this.classList.add('flipped');
+                this.nextElementSibling.classList.add('flipped');
+              }
+           }
+        }
+  })
