d2fbcc940503a44c237be9eb841b10912c903abe
Yaroslav Kaspryshyn <yaroslav.kaspryshyn@easybrain.com>
2020-06-29 23:24:53 +0300

    removed video autoplay

--- script.js
+++ script.js
@@ -18,13 +18,35 @@ var pages = document.getElementsByClassName('page');
             if (this.pageNum % 2 === 0)
               {
                 this.classList.remove('flipped');
+                var currentVideo = this.getElementsByClassName('sliderVideo')[0];
+                currentVideo.pause();
+
                 this.previousElementSibling.classList.remove('flipped');
+                var nextVideo = this.previousElementSibling.getElementsByClassName('sliderVideo')[0];
+                nextVideo.play();
+                
+                if (this.previousElementSibling.previousElementSibling){
+                  var nextNextVideo = this.previousElementSibling.previousElementSibling.getElementsByClassName('sliderVideo')[0];
+                  nextNextVideo.play();
+                }
               }
             else
               {
                 this.classList.add('flipped');
+                var currentVideo = this.getElementsByClassName('sliderVideo')[0];
+                currentVideo.pause();
+
                 this.nextElementSibling.classList.add('flipped');
+                var nextVideo = this.nextElementSibling.getElementsByClassName('sliderVideo')[0];
+                nextVideo.play();
+                
+                if (this.nextElementSibling.nextElementSibling){
+                  var nextNextVideo = this.nextElementSibling.nextElementSibling.getElementsByClassName('sliderVideo')[0];
+                  nextNextVideo.play();
+                }
               }
            }
         }
+        var firstVideo = pages[0].getElementsByClassName('sliderVideo')[0];
+        firstVideo.play();
   })
